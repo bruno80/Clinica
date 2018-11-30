017d6cd309b6887ce3ab12a35e2aa496f3825eb2
Aluno <you@example.com>
2018-11-30 04:43:56 -0300

    criação dos detalhes do médico

--- atendimento/urls.py
+++ atendimento/urls.py
@@ -4,5 +4,7 @@ from . import views
 urlpatterns = [
     path('', views.home),
     path('cliente/', views.list_cliente),
-    path('medico/', views.list_medico)
+    path('medico/', views.list_medico),
+    path('cliente/<int:cliente_id>/',views.cliente_show),
+    path('medico/<int:medico_id>/',views.medico_show)
 ]
--- atendimento/views.py
+++ atendimento/views.py
@@ -6,9 +6,17 @@ def home(request):
     return render(request, 'home.html', {})
 
 def list_cliente(request):
-    clientes = Cliente.objects.all
-    return render(request, 'cliente/cliente.html', {'clientes':clientes})
+    clientes = Cliente.objects.all()
+    return render(request, 'cliente/list.html', {'clientes':clientes})
 
 def list_medico(request):
-    medicos = Medico.objects.all
-    return render(request, 'medico/medico.html', {'medicos':medicos})
+    medicos = Medico.objects.all()
+    return render(request, 'medico/list.html', {'medicos':medicos})
+
+def cliente_show(request, cliente_id):
+    cliente = Cliente.objects.get(pk=cliente_id)
+    return render(request, 'cliente/show.html', {'cliente':cliente})
+
+def medico_show(request, medico_id):
+    medico = Medico.objects.get(pk=medico_id)
+    return render(request, 'medico/show.html', {'medico':medico})
